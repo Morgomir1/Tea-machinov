b3ce5f3443a2da427ceea21029e75224dffb5306
Egor Timoshinov <egor.timoshinov.02@mail.ru>
2025-11-13 07:47:27 +0300

    Initial Commit

---
+++ Tea-machinov/Tea_machinovApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tea_machinovApp.swift
+//  Tea-machinov
+//
+//  Created by user on 13.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Tea_machinovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
